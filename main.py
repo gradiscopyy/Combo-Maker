f85898a4e5ad646babe9aebfe0edc80280866392
! gradiscopy <gradiscopy@gmail.com>
2024-07-11 00:57:10 +0300

    Update main.py
    added dirs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
+
 def read_file(filename):
     exclude_message = "Scraped by Kyanite - https://idiotide.sellix.io/"
     with open(filename, 'r') as file:
         return [line.strip() for line in file.readlines() if line.strip() != exclude_message]
-
 
 def write_combos(usernames, passwords, output_filename):
     with open(output_filename, 'w') as file:
@@ -11,9 +12,17 @@ def write_combos(usernames, passwords, output_filename):
                 file.write(f"{username}:{password}\n")
 
 def main():
-    usernames = read_file('usernames.txt')
-    passwords = read_file('passwords.txt')
-    write_combos(usernames, passwords, 'combos.txt')
+    input_dir = 'input'
+    output_dir = 'output'
+    
+    os.makedirs(input_dir, exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
+    
+    usernames = read_file(os.path.join(input_dir, 'usernames.txt'))
+    passwords = read_file(os.path.join(input_dir, 'passwords.txt'))
+    
+    write_combos(usernames, passwords, os.path.join(output_dir, 'combos.txt'))
+    
     total_combos = len(usernames) * len(passwords)
     input(f"[+] {total_combos}/{total_combos} Combos were made")
 
@@ -21,4 +30,4 @@ if __name__ == "__main__":
     main()
 
 
-# made by @gradiscopyyy
+#made by gradiscopy
